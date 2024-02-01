c27873edf6f9ee11e4cf816d1e84f14849b2bb1e
Matthieu92b <133603299+Matthieu92b@users.noreply.github.com>
2024-02-01 09:20:43 +0100

    AVL.h

---
+++ AVL.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+
+pArbre creationArbre_T(char* nomVilleCourante, int IdrouteCourante,int stepIdcourant, bool premiereEtape);
+pArbre creationArbre_S(int IdrouteCourante,int stepIdcourant,  float distance);pArbre rotationGauche(pArbre a);
+pArbre rotationGauche(pArbre a);
+pArbre rotationDroite(pArbre a);
+pArbre doubleRotationGauche(pArbre a);
+pArbre doubleRotationDroite(pArbre a);
+pArbre equilibreAVL(pArbre a);
+pArbre insertVille(pArbre a, char* nomVilleCourante, int RouteIdCourante,int stepIdcourant ,bool premiereEtape, int *h);
+pArbre insertRoute(pArbre a,int RouteIdCourante,int stepIdcourant, float distance, int *h);
+
