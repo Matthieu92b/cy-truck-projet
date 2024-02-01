42ebbdf924d4b81a42a8f152d7155479b25a7a5d
Matthieu92b <133603299+Matthieu92b@users.noreply.github.com>
2024-02-01 09:21:54 +0100

    IHM.h

---
+++ IHM.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+
+
+
+void AfficherResultat_T();
+void AfficherResultat_S();
+void freeArbre_T(pArbre a) ;
+void freeArbre_S(pArbre a) ;
+
