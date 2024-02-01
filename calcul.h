998fcd32c2292f6a12550a0fade27185fd54cb43
Matthieu92b <133603299+Matthieu92b@users.noreply.github.com>
2024-02-01 09:19:42 +0100

    calcul.h

---
+++ calcul.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+
+
+
+int minRotGauche(int a, int b, int c);
+int maxRotGauche(int a, int b);
+int minRotDroite(int a, int b);
+int maxRotDroite(int a, int b, int c);
+void rechercheVille(pArbre a);
+void rechercheRoute(pArbre a);
